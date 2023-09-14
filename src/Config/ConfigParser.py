38a4d6d35df9809c95c12f06b533385cba6d04c1
Pritam Sarkar <pritamps2002@gmail.com>
2023-09-14 16:15:37 +0530

    Handled exceptions and updated TODO

--- src/Config/ConfigParser.py
+++ src/Config/ConfigParser.py
@@ -1,10 +1,12 @@
 import json
+import sys
 
 class ConfigParser:
     def __init__(self, platform_name):
         self.config_file_path = "Config.json"
         self.platform_name = platform_name.capitalize()
         self.config_file_data = None
+
         # reading config file data - 
         try:
             file = open(self.config_file_path)
@@ -18,12 +20,15 @@ class ConfigParser:
             
             if self.config_file_data is None:
                 raise Exception(f"Config for Platform: {self.platform_name} not found!")
-      
-        except Exception as e:
-            print('Error:', e)
-            exit(1)
 
-    def check_config(self):
+            self.__check_config()
+
+        except Exception as e:
+            print(f"Error loading Config: {e}")
+            sys.exit(1)
+
+    def __check_config(self):
+
         # precheck parameters -
         required_params = [
             "language_extension",
@@ -43,11 +48,9 @@ class ConfigParser:
         if "required" in self.config_file_data:
             for parameter in required_params:
                 if parameter not in self.config_file_data["required"] or self.config_file_data["required"][parameter] == "":
-                    print(f"Required Parameter: {parameter} for Platform: {self.platform_name} not defined!")
-                    exit(1)
+                    raise Exception(f"Required Parameter: {parameter} for Platform: {self.platform_name} not defined!")
         else:
-            print(f"Required Config for Platform: {self.platform_name} not defined!")
-            exit(1)
+            raise Exception(f"Required Config for Platform: {self.platform_name} not defined!")
 
         # updating optional parameters -
         if "optional" in self.config_file_data:
@@ -59,8 +62,8 @@ class ConfigParser:
             self.config_file_data['optional'] = optional_params
         
         # print(json.dumps(self.config_file_data, indent=2))
-        return self.config_file_data
+        # return self.config_file_data
         
 # Debugging      
 # obj = ConfigParser('atcoder')
-# data = obj.check_config()
+# print(obj.config_file_data)
